462295a1fd0cd315475616c625dc37e543de9a79
Erik Müller <erik.mueller@power.cloud>
2024-05-22 11:41:25 +0200

    Feature: handle pull requests origin

--- src/main/java/io/jenkins/plugins/multibranch/BranchPriorityStrategy.java
+++ src/main/java/io/jenkins/plugins/multibranch/BranchPriorityStrategy.java
@@ -4,6 +4,8 @@ import hudson.Extension;
 import hudson.model.Job;
 import hudson.model.Queue;
 import jenkins.advancedqueue.priority.strategy.AbstractDynamicPriorityStrategy;
+import jenkins.branch.Branch;
+import jenkins.scm.api.mixin.ChangeRequestSCMHead2;
 import org.jenkinsci.plugins.workflow.multibranch.BranchJobProperty;
 import org.kohsuke.stapler.DataBoundConstructor;
 
@@ -17,11 +19,13 @@ public class BranchPriorityStrategy extends AbstractDynamicPriorityStrategy {
     };
 
     private String branchName;
+    private Boolean pullRequestMatchOriginName;
     private int priority;
 
     @DataBoundConstructor
-    public BranchPriorityStrategy(String branchName, int priority) {
+    public BranchPriorityStrategy(String branchName, Boolean pullRequestMatchOriginName, int priority) {
         this.branchName = branchName;
+        this.pullRequestMatchOriginName = pullRequestMatchOriginName;
         this.priority = priority;
     }
 
@@ -35,8 +39,16 @@ public class BranchPriorityStrategy extends AbstractDynamicPriorityStrategy {
         if(item.task instanceof Job<?, ?>) {
             Job<?, ?> job = (Job<?, ?>) item.task;
             BranchJobProperty priorityProperty = job.getProperty(BranchJobProperty.class);
-            if (priorityProperty != null && priorityProperty.getBranch().getName().matches(branchName)) {
-                return priority;
+            if (priorityProperty != null) {
+                Branch branch = priorityProperty.getBranch();
+                String originName = branch.getName();
+                if (pullRequestMatchOriginName && branch.getHead() instanceof ChangeRequestSCMHead2) {
+                    originName = ((ChangeRequestSCMHead2) branch.getHead()).getOriginName();
+                }
+
+                if (originName.matches(branchName)) {
+                    return priority;
+                }
             }
         }
         return 3;
@@ -44,6 +56,10 @@ public class BranchPriorityStrategy extends AbstractDynamicPriorityStrategy {
 
     public String getBranchName() {
         return branchName;
+    }
+
+    public Boolean getPullRequestMatchOriginName() {
+        return pullRequestMatchOriginName;
     }
 
     public int getPriority() {
